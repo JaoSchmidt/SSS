cbb4065ce9bf7158578cf28285a870d760500b34
JaoSchmidt <jhsc98@gmail.com>
2021-12-01 00:34:31 -0300

    vim templates

---
+++ .vim/templates/skel.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+export default {
+	data: function(){
+		return {};
+	},
+	components : {  },
+	mounted : function(){  },
+	methods : {  },
+}
+</script>
+
+<style>
+
+</style>
